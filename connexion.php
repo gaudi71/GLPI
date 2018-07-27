b4ca57ecc88c303b60a6e7eada52770d3a48c925
Haribo20 <41804575+Haribo20@users.noreply.github.com>
2018-07-27 22:05:05 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,8 +1,21 @@
- try {
-        $bdd = new PDO('mysql:host=127.0.0.1;dbname=glpi_parc', 'root', 'root');
-    } catch (Exception $e) {
-        echo 'Exception reçue : ', $e->getMessage(), "\n";
-        $bdd->CloseCursor;
-    }
-    }
-?>
+function getDataBaseLocal(){
+	// chargement du fichier ini
+	$ini = parse_ini_file("Connexion.ini",true);
+	
+	// Accès à la base de données			
+	$bdd_servername = $ini['bdd_Local']['bdd_servername'];
+	$bdd_username = $ini['bdd_Local']['bdd_username'];
+	$bdd_password = $ini['bdd_Local']['bdd_password'];
+	$bdd_dbname = $ini['bdd_Local']['bdd_dbname'];
+
+	try {
+		$bdd = new PDO("mysql:host=$bdd_servername;dbname=$bdd_dbname", $bdd_username, $bdd_password);
+		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$bdd->query("set names utf8");
+	}
+	catch(PDOException $e)
+	{
+		die("Connection failed: " . $e->getMessage());
+	}
+	return $bdd;
+}
